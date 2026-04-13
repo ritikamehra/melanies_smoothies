b235ed6662943e178b3af82f386bf020ee487030
ritikamehra <54076372+ritikamehra@users.noreply.github.com>
2026-04-13 13:29:27 +0530

    Update streamlit_app.py
    Added search_on col in select

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be: ", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
